b2d3f9ec1e1427c66fe463481719e7b4f54d0c94
Isaac Lopez PC <isaaclopezprog@gmail.com>
2022-02-03 14:40:09 -0400

    avance 6, finalizacion del detalle de inicio

--- ondasprofeticas/src/components/inicio/Detalle.js
+++ ondasprofeticas/src/components/inicio/Detalle.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 
 // EXTERNAL
 import Image from 'next/image';
+import {Tab} from '@headlessui/react';
 
 // IMG
 import foto from '../../../public/img/foto.jpg'
 import divider from '../../../public/img/divider.png'
+
+// ICONS
+
+import { ArrowRightIcon, ArrowLeftIcon } from '@heroicons/react/outline'
 
 export default function Detalle(){
 
@@ -19,7 +24,7 @@ export default function Detalle(){
                     <br/>
                     PROFETICAS
                     </h1>
-                    <div className="basis-2/3 mb-12">
+                    <div className="mb-12">
                         <Image 
                             src={divider}
                             objectFit="responsive"
@@ -27,23 +32,52 @@ export default function Detalle(){
                             quality={100}
                         />
                     </div>
-                    <div className="basis-1/4 mb-12">
-                        
-                    </div>
-                    <div className="">
-                        <h5 className="text-xl font-light my-4">Somos un ministerio dedicado a predicar la palabra de manera objetiva.</h5>
-                            <p className="font-light text-base">Llevando el mensaje de Jesucristo a través de:</p>
-                            <p className="text-slate-500 font-light"> - Predicaciones en linea y presenciales.</p>
-                            <p className="text-slate-500 font-light"> - Capacitaciones a líderes.</p>
-                            <p className="text-slate-500 font-light"> - Conferencias para desarrollo personal, familiar y ministerial.</p>
-                            <p className="text-slate-500 font-light"> - Obras sociales.</p>
-                    </div>
-                    <div>
-                        <h5> Nuestro llamado está basado en la palabra dicha en:</h5>
-                        <p class="black"> <span style={{borderBottom: "1px solid black"}}> JEREMIAS 1:10 </span>
-                        Mira que te he puesto en este día sobre naciones y sobre reinos, para arrancar y para destruir, para arruinar y para derribar, para edificar y para plantar. </p>
-                        <p>  Y Actualmente predicamos la palabra a fin de arrancar, destruir, arruinar y derribar todo argumento que se levanté en contra del conocimiento de Cristo y así poder edificar y plantar la semilla del evangelio en cada persona que nos escuche.</p>
-        
+                    <div className="tab">
+                        <Tab.Group>
+                            <Tab.List >
+                                <Tab.Panels>
+                                    <Tab.Panel>
+                                        <div className="detalle-1 mb-8">
+                                            <h5 className="text-xl font-light my-4">Somos un ministerio dedicado a predicar la palabra de manera objetiva.</h5>
+                                                <p className="font-light text-base">Llevando el mensaje de Jesucristo a través de:</p>
+                                                <p className="text-slate-500 font-light"> - Predicaciones en linea y presenciales.</p>
+                                                <p className="text-slate-500 font-light"> - Capacitaciones a líderes.</p>
+                                                <p className="text-slate-500 font-light"> - Conferencias para desarrollo personal, familiar y ministerial.</p>
+                                                <p className="text-slate-500 font-light"> - Obras sociales.</p>
+                                        </div>
+                                    </Tab.Panel>
+                                <Tab.Panel>
+                                <div className="detalle-2 mb-8">
+                                    <h5 className="text-xl font-light my-4"> Nuestro llamado está basado en la palabra dicha en:</h5>
+                                    <p className="font-light text-base"> <span className="border-b border-black"> JEREMIAS 1:10 </span>
+                                    Mira que te he puesto en este día sobre naciones y sobre reinos, para arrancar y para destruir, para arruinar y para derribar, para edificar y para plantar. </p>
+                                    <p className="text-slate-500 font-light">  Y Actualmente predicamos la palabra a fin de arrancar, destruir, arruinar y derribar todo argumento que se levanté en contra del conocimiento de Cristo y así poder edificar y plantar la semilla del evangelio en cada persona que nos escuche.</p>
+                                </div>
+                            </Tab.Panel>
+                                </Tab.Panels>
+                                <Tab as={Fragment}>
+                            {({ selected}) =>(
+                                <button className={
+                                    selected ? 'hidden ' : 'mr-2 ml-24 md:ml-40 bg-white text-amber-400 hover:text-white hover:bg-black'
+                                }>
+                                    <ArrowLeftIcon className="h-auto w-12"/>
+                                </button>
+                            )}
+                        </Tab>
+                                <Tab as={Fragment} >
+                            {({ selected}) =>(
+                                <button className={
+                                    selected ? 'hidden' : ' mr-2 ml-24 md:ml-40 bg-white text-amber-400 hover:text-white hover:bg-black'
+                                }>
+                                    <ArrowRightIcon className="h-auto w-12"/>
+                                </button>
+                            )}
+                        </Tab>
+                                {/* <Tab className="p-4 bg-gray-50 text-center text-lg font-bold active:text-amber-400 hover:text-white hover:bg-gray-900">
+                                    <ArrowRightIcon className="h-5 w-5  "/>
+                                </Tab> */}
+                            </Tab.List>
+                        </Tab.Group>
                     </div>
                 </div>
             </div>
@@ -57,8 +91,6 @@ export default function Detalle(){
                     />
                 </div>
             </div>
-
-
         </div>
         </>
     )
