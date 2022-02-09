e784c057d4a13a9f1aee5b44fa71468dd969b202
Isaac Lopez PC <isaaclopezprog@gmail.com>
2022-02-09 09:58:31 -0400

    avance 15, test con vercel

--- ondasprofeticas/src/components/donacion/Conocenos.js
+++ ondasprofeticas/src/components/donacion/Conocenos.js
@@ -4,11 +4,77 @@ import React from 'react';
 import divider from '../../../public/img/divideriglesia2.png';
 import divider2 from '../../../public/img/divideraccion.png';
 
+import accionp from '../../../public/img/accionp.jpg';
+import accionp2 from '../../../public/img/accionp2.jpg';
+import accionp3 from '../../../public/img/accionp3.jpg';
+import accionp4 from '../../../public/img/accionp4.jpg';
+
 // EXTERNAL
 import Image from 'next/image';
 
 
 export default function Conocenos(){
+
+
+    const publicacion = [
+        { 
+            tema:'Cuarta Prueba',
+            lugar:'Santiago',
+            descripcion:'Desde un punto de vista técnico, el concepto de educación describe a un proceso continuo, en el que se desarrollan las facultades intelectuales, morales y físicas del hombre, con el objetivo de que se incorpore de manera eficiente en la sociedad o en el grupo donde se desenvuelve, por tanto, se puede decir que es un aprendizaje para la vida. Por otro lado, cuando de estudio formal se refiere, cabe acotar que se trata del proceso educativo de cada individuo, y por tanto es considerado un derecho fundamental y obligatorio de los seres humanos, por lo que debe ser garantizado por parte de los gobiernos de cada país.',
+            fecha:'20/12/20',
+            facebook:'http://www.facebook.com',
+            instagram:'http://instagram.com',
+            youtube:'http://www.youtube.com',
+            video:'https://www.youtube.com/embed/pOKnOk-jaTk',
+            foto1:accionp,
+            foto2:accionp2,
+            foto3:accionp3,
+            foto4:accionp4
+        },
+        { 
+            tema:'Segunda Prueba',
+            lugar:'Santiago',
+            descripcion:'Desde un punto de vista técnico, el concepto de educación describe a un proceso continuo, en el que se desarrollan las facultades intelectuales, morales y físicas del hombre, con el objetivo de que se incorpore de manera eficiente en la sociedad o en el grupo donde se desenvuelve, por tanto, se puede decir que es un aprendizaje para la vida. Por otro lado, cuando de estudio formal se refiere, cabe acotar que se trata del proceso educativo de cada individuo, y por tanto es considerado un derecho fundamental y obligatorio de los seres humanos, por lo que debe ser garantizado por parte de los gobiernos de cada país.',
+            fecha:'20/12/20',
+            facebook:'http://www.facebook.com',
+            instagram:'http://instagram.com',
+            youtube:'http://www.youtube.com',
+            video:'https://www.youtube.com/embed/pOKnOk-jaTk',
+            foto1:accionp,
+            foto2:accionp2,
+            foto3:accionp3,
+            foto4:accionp4
+        },
+        { 
+            tema:'Tercera Prueba',
+            lugar:'Santiago',
+            descripcion:'Desde un punto de vista técnico, el concepto de educación describe a un proceso continuo, en el que se desarrollan las facultades intelectuales, morales y físicas del hombre, con el objetivo de que se incorpore de manera eficiente en la sociedad o en el grupo donde se desenvuelve, por tanto, se puede decir que es un aprendizaje para la vida. Por otro lado, cuando de estudio formal se refiere, cabe acotar que se trata del proceso educativo de cada individuo, y por tanto es considerado un derecho fundamental y obligatorio de los seres humanos, por lo que debe ser garantizado por parte de los gobiernos de cada país.',
+            fecha:'20/12/20',
+            facebook:'http://www.facebook.com',
+            instagram:'http://instagram.com',
+            youtube:'http://www.youtube.com',
+            video:'https://www.youtube.com/embed/pOKnOk-jaTk',
+            foto1:accionp,
+            foto2:accionp2,
+            foto3:accionp3,
+            foto4:accionp4
+        },
+        { 
+            tema:'Quinta Prueba',
+            lugar:'Santiago',
+            descripcion:'Desde un punto de vista técnico, el concepto de educación describe a un proceso continuo, en el que se desarrollan las facultades intelectuales, morales y físicas del hombre, con el objetivo de que se incorpore de manera eficiente en la sociedad o en el grupo donde se desenvuelve, por tanto, se puede decir que es un aprendizaje para la vida. Por otro lado, cuando de estudio formal se refiere, cabe acotar que se trata del proceso educativo de cada individuo, y por tanto es considerado un derecho fundamental y obligatorio de los seres humanos, por lo que debe ser garantizado por parte de los gobiernos de cada país.',
+            fecha:'20/12/20',
+            facebook:'http://www.facebook.com',
+            instagram:'http://instagram.com',
+            youtube:'http://www.youtube.com',
+            video:'https://www.youtube.com/embed/pOKnOk-jaTk',
+            foto1:accionp,
+            foto2:accionp2,
+            foto3:accionp3,
+            foto4:accionp4
+        }
+    ]
+
 
     return(
         <>
@@ -21,6 +87,8 @@ export default function Conocenos(){
                         <Image src={divider} />
                     </div>
                     <div className="CONTENIDO w-full" >
+                    {publicacion.map(post =>(
+            
                         <div className="PADRE md:ml-36 p-8 mt-8 ">
                             <div className="FECHA  absolute w-24 h-24 text-base text-center mt-24 -ml-12 z-10 bg-amber-400">
                                 <span className="text-4xl font-bold block text-center py-2">22</span>
@@ -29,17 +97,19 @@ export default function Conocenos(){
                                 </p>
                             </div>
                             <div className="VIDEO mb-8">
-                                <iframe width="560" height="315" src="https://www.youtube.com/embed/pOKnOk-jaTk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <iframe width="560" height="315" src={post.video} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
                             <div className="TEMA ">
-                                <h1 className="uppercase font-light text-4xl text-center md:text-left -mb-2">cuarta prueba</h1>
+                                <h1 className="uppercase font-light text-4xl text-center md:text-left -mb-2">{post.tema}</h1>
                                 <Image src={divider2} />
-                                <p className="font-light max-w-4xl text-gray-500 mt-4">Desde un punto de vista técnico, el concepto de educación describe a un proceso continuo, en el que se desarrollan las facultades intelectuales, morales y físicas del hombre, con el objetivo de que se incorpore de manera eficiente en la …</p>
+                                <p className="font-light max-w-4xl text-gray-500 mt-4">{post.descripcion}</p>
                                 <button className="divide-y-4 uppercase font-medium mt-8 border-2 border-amber-400 py-2 px-4 rounded hover:bg-amber-400">ver mas</button>
                             </div>
                             <div className="w-full bg-gray-500 mt-8" style={{height:'2px'}}></div>
 
                         </div>
+
+        ))}
                     </div>
                 </div>
             </div>
--- ondasprofeticas/src/components/layout/Header.js
+++ ondasprofeticas/src/components/layout/Header.js
@@ -10,7 +10,7 @@ const Header = props =>{
             <Head>
             <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'/>
             <link href='https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'/>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
             <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
             <script src="https://cdn.tailwindcss.com"></script>
             <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
