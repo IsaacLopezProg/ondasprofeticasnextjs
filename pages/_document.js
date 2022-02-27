db718b4ff066d085b2f69b7389e6ce63b5fc909b
Isaac Lopez PC <isaaclopezprog@gmail.com>
2022-02-27 18:30:33 -0400

    39 avance, agregado del lang y del sitemap para bing

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="es">
         <Head>
             <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
         </Head>
